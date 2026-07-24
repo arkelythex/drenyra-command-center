2344e20c31c44e0226541d5a491900f1fb90df27
DreamCoder08 <agurtofarfanalbert@gmail.com>
2026-07-24 14:06:08 -0500

    feat(web): add FiscalInspectorProvider, repo-health/tools  routes, and biome ignore for generated route tree

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,14 +1,23 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import * as React from "react";
+import { createRootRoute, Outlet, useLocation } from "@tanstack/react-router";
+import { FiscalInspectorProvider } from "@/context/FiscalInspectorContext";
+import { isPublicRoute } from "@/lib/router/public-routes";
 
 export const Route = createRootRoute({
 	component: RootComponent,
 });
 
 function RootComponent() {
+	const pathname = useLocation({
+		select: (location) => location.pathname,
+	});
+
+	if (isPublicRoute(pathname)) {
+		return <Outlet />;
+	}
+
 	return (
-		<React.Fragment>
+		<FiscalInspectorProvider>
 			<Outlet />
-		</React.Fragment>
+		</FiscalInspectorProvider>
 	);
 }
